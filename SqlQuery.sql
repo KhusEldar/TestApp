232e0229debe53ffccc1f44efb4685cb9be52474
Эльдар Хуснутдинов <eldar.husnutdinov@gmail.com>
2024-09-03 21:23:49 +0300

    Add SQL query

---
+++ SqlQuery.sql
@@ -0,0 +1,4 @@
+﻿select product_name = p.name, category_name = c.name
+from product p --Продукты
+left join product_category pc on pc.product_id = p.id --Таблица для связи продуктов и категорий (многие ко многим)
+left join category c  on c.id = pc.category_id --Категории
